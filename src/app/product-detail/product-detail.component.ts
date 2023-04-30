9098a17b9938243f005ec149bf4112a291a8a97d
mojtabanikahd <mojtabanikahd@gmail.com>
2023-04-30 17:49:44 +0430

    Make the product detail responsive

--- src/app/product-detail/product-detail.component.ts
+++ src/app/product-detail/product-detail.component.ts
@@ -3,6 +3,8 @@ import { ActivatedRoute } from "@angular/router";
 import { Product } from "../../model/product";
 import { ProductService } from "../product.service";
 import { Location } from "@angular/common";
+import { BreakpointObserver, Breakpoints } from "@angular/cdk/layout";
+import { map } from "rxjs";
 
 @Component({
   selector: 'app-product-detail',
@@ -11,12 +13,36 @@ import { Location } from "@angular/common";
 })
 export class ProductDetailComponent {
   product: Product | undefined;
+  colNum: number = 2;
+  math = Math;
 
   constructor(
     private productService: ProductService,
     private route: ActivatedRoute,
-    private location: Location
+    private location: Location,
+    private breakpointObserver: BreakpointObserver
   ) {
+    this.breakpointObserver.observe([
+      Breakpoints.XSmall,
+      Breakpoints.Small,
+      Breakpoints.Medium,
+      Breakpoints.Large,
+      Breakpoints.XLarge
+    ]).pipe(
+      map(({ breakpoints }) => {
+        if (breakpoints[Breakpoints.XSmall])
+          this.colNum = 1;
+        else if (breakpoints[Breakpoints.Small])
+          this.colNum = 1;
+        else if (breakpoints[Breakpoints.Medium])
+          this.colNum = 2;
+        else if (breakpoints[Breakpoints.Large])
+          this.colNum = 3;
+        else if (breakpoints[Breakpoints.XLarge])
+          this.colNum = 3;
+        return 2;
+      })
+    ).subscribe();
   }
 
   ngOnInit(): void {
@@ -31,5 +57,9 @@ export class ProductDetailComponent {
 
   goBack() {
     this.location.back();
+  }
+
+  delete(id: number) {
+    this.productService.deleteProduct(id).subscribe( () => this.goBack());
   }
 }
