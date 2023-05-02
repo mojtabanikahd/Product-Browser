17631f02fbf67f16de56201899131deeb23fc746
mojtabanikahd <mojtabanikahd@gmail.com>
2023-05-02 16:38:58 +0430

    create separate folders for frontend and backend

---
+++ backend/src/index.ts
@@ -0,0 +1,130 @@
+import { Prisma, PrismaClient } from '@prisma/client'
+import express from 'express'
+
+const jwt = require("jsonwebtoken");
+
+const prisma = new PrismaClient()
+const app = express()
+
+app.use(express.json())
+
+
+// JWT Handler
+//Passed
+app.post("/login", async (req, res, next) => {
+  let { username, password } = req.body;
+  
+  if (username != "test" || password != "test") {
+	const error = new Error("Error! Something went wrong.")
+    return next(error)
+  } else {
+	let token;
+  try {
+    //Creating jwt token
+    token = jwt.sign(
+      { userId: "test"},
+      "secretkeyappearshere",
+      { expiresIn: "1h" }
+    );
+  } catch (err) {
+    console.log(err);
+    const error = new Error("Error! Something went wrong.");
+    return next(error);
+  }
+ 
+  res.status(200).json({
+      success: true,
+      data: {
+        userId: "test",
+        token: token,
+      },
+    });
+  }
+});
+
+app.post(`/product`, (req, res, next)=> {  
+	let token = req.headers["x-access-token"];
+    //Authorization: 'Bearer TOKEN'
+    if(!token)
+    {
+        res.status(403).json({success:false, message: "Error! Token was not provided."});
+    }
+    //Decoding the token
+	try{
+		const decodedToken = jwt.verify(token,"secretkeyappearshere" );
+	} catch (err) {
+		return res.status(401).send("Invalid Token");
+	}
+	next();
+})
+
+
+
+// RESTfull API for CRUD operations
+app.post(`/product`, async (req, res) => {
+  const { name, description, image } = req.body
+  
+  const result = await prisma.product.create({
+    data: {
+      name,
+      description,
+      image,
+	},
+  })
+  res.json(result)
+})
+
+//Passed
+app.put('/product/:id', async (req, res) => {
+  const { id } = req.params
+  const { name, description, image } = req.body
+
+  try {
+    const updatedProduct = await prisma.product.update({
+      where: { id: Number(id) },
+      data: {
+        name: String(name),
+		description: String(description),
+		image: image,
+      },
+    })
+
+    res.json(updatedProduct)
+  } catch (error) {
+    res.json({ error: `Product with ID ${id} does not exist in the database` })
+  }
+})
+
+//Passed
+app.delete(`/product/:id`, async (req, res) => {
+  const { id } = req.params
+  const product = await prisma.product.delete({
+    where: {
+      id: Number(id),
+    },
+  })
+  res.json(product)
+})
+
+//Passed
+app.get('/products', async (req, res) => {
+  const products = await prisma.product.findMany()
+  res.json(products)
+})
+
+//Passed
+app.get('/product/:id', async (req, res) => {
+  const { id } = req.params
+
+  const product = await prisma.product.findUnique({
+    where: { id: Number(id) },
+  })
+  res.json(product)
+})
+
+
+const server = app.listen(3000, () =>
+  console.log(`
+🚀 Server ready at: http://localhost:3000
+⭐️ See sample requests: http://pris.ly/e/ts/rest-express#3-using-the-rest-api`),
+)
